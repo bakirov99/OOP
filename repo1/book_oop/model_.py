91fcf1a96fe4c02a0455ce83cf40e2fea0ad9e00
Timur <btimur.mail@gmail.com>
2024-08-19 16:20:18 +0500

    qayta o'rganish uchun 1

--- repo1/book_oop/model_.py
+++ repo1/book_oop/model_.py
@@ -1,4 +1,23 @@
-from typing import cast
+import datetime
+from typing import cast, Set, Iterable
+from enum import Enum
+
+
+class Species(Enum):
+    Setosa = "Iris-setosa"
+    Versicolour = 'Iris-versicolour'
+    Virginica = 'Iris-virginica'
+
+
+class Hyperparameter:
+    pass
+
+
+class Domain(Set[str]):
+    def validate(self, value: str) -> str:
+        if value in self:
+            return value
+        raise ValueError(f'invalid {value!r}')
 
 
 class InvalidSampleError(ValueError):
@@ -28,11 +47,9 @@ class KnowSample(Sample):
 
     @classmethod
     def from_dict(cls, row: dict[str, str]) -> "KnowSample":
-        if row['species'] not in ("Iris-setosa", "Iris-versicolour",
-                                  "Iris-virginica"):
-            raise InvalidSampleError(f"Invalid species in {row!r}")
+        species = Domain()
         try:
-            return cls(species=row['species'], sepal_length=float(row['sepal_length']),
+            return cls(species=species.validate(row['species']), sepal_length=float(row['sepal_length']),
                        sepal_width=float(row['sepal_width']), petal_length=float(row['petal_length']),
                        petal_width=float(row['petal_length']), )
         except ValueError as ex:
@@ -53,6 +70,35 @@ class TestingKnowSample(KnowSample):
     @classmethod
     def from_dict(cls, row: dict[str, str]) -> "TestingKnowSample":
         return cast(TestingKnowSample, super().from_dict(row))
+
+
+class TrainingData:
+
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self.uploaded = datetime.datetime
+        self.tested: datetime.datetime
+        self.training: list[TrainingKnowSample] = []
+        self.testing: list[TestingKnowSample] = []
+        self.tuning: list[Hyperparameter] = []
+
+    def load(self, raw_data_iter: Iterable[dict[str, str]]) -> None:
+        bad_count = 0
+        for n, row in enumerate(raw_data_iter):
+            try:
+                if n % 5 == 0:
+                    test = TestingKnowSample.from_dict(row)
+                    self.testing.append(test)
+                else:
+                    train = TrainingKnowSample.from_dict(row)
+                    self.training.append(train)
+            except InvalidSampleError as ex:
+                print(f"Row {n+1}: {ex}")
+                bad_count += 1
+        if bad_count != 0:
+            print(f"{bad_count} invalid rows")
+            return
+        self.uploaded = datetime.datetime.now(tz=datetime.timezone.utc)
 
 
 if __name__ == '__main__':
--- repo1/book_oop/practical.py
+++ repo1/book_oop/practical.py
@@ -1,14 +1,10 @@
-from model_ import TestingKnowSample, InvalidSampleError
+from model_ import Domain
 
 
 if __name__ == '__main__':
-    invalid_species = {'sepal_length': '5.1', 'sepal_width': '3.5',
-                       'petal_length': '1.4', 'petal_width': '0.2',
-                       'species': "nothing known by this app"}
-    rks = TestingKnowSample.from_dict(invalid_species)
-    print(rks)
-
-
+    species = Domain({"Iris-setosa", 'Iris-versicolour', 'Iris-virginica'})
+    print(species.validate("Iris-versicolour"))
+    print(species.validate('Odobenidae'))
 
 
 
